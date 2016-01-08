457c008656af911a01be8f6f17e8bf7b9853dfe9
Dewan Mahabub Sarwar <sarwarcse@gmail.com>
2016-01-08 15:54:50 +1300

    removed redundant edges in EdgeList

--- index.js
+++ index.js
@@ -6,8 +6,6 @@
 var TempList = [];
 
 var SMT = function () {
-    //var previousfitness = 0;
-    //var fitnessunchanged = 0;
 
     var genetic = Genetic.create();
 
@@ -47,8 +45,8 @@ var SMT = function () {
             }
         }
 
-        console.log(TempList);
-        console.log(this.RequiredNodeList);
+        console.log("TempList: ", TempList);
+        console.log("RequiredNodeList: ", this.RequiredNodeList);
 
         genetic.chromos = function (path) {
             var myEdges = [];
@@ -158,12 +156,6 @@ var SMT = function () {
                 return this.ourchromosomes[this.index - 1];
             }
         }
-        //console.log("SEED IF this.chromosomes", this.chromosomes2, this.len);
-        //console.log("chromosome: ", this.chromosomes2[this.c]);
-        //
-        //this.c++;
-        //return this.chromosomes2[this.c - 1]; //this.chromosomes2.shift();
-
         /*
          * Again, seed is the combination of our set of chromosomes
          * That means, all index is 1 in chromosomes array
@@ -174,26 +166,7 @@ var SMT = function () {
 
             console.log("Odd, Combined chromosome: ", mixchromosomes);
             return mixchromosomes;
-
-            //console.log("SEED ELSE");
-            //
-            //var chromosome = [];
-            //var len = this.userData["edges"].length;
-            ////console.log("EdgeList len SEED: ", len);
-            //var i = Math.floor(Math.random() * len);
-            //
-            //for (var j = 0; j < len; j++) {
-            //    var i = Math.floor(Math.random() * len);
-            //    if (i % 2 == 0)
-            //        chromosome[j] = 1;
-            //    else
-            //        chromosome[j] = 0;
-            //}
-            //
-            //return chromosome;
         }
-
-        //return this.chromosomes;
     }
 
     /*
@@ -242,13 +215,10 @@ var SMT = function () {
         return [son, daughter];
     }
 
-    genetic.ourfitnesscalc = function (chromosome) {
+    genetic.OurFitnessFunction = function (chromosome) {
 
-        console.log("ourfitness chromosome: ", chromosome);
+        console.log("OurFitness chromosome: ", chromosome);
         var entity = chromosome;
-
-        //console.log("Fitness function ****************** ");
-        //console.log("Chromosome: " + entity);
 
         var fitness = 0;
 
@@ -261,7 +231,6 @@ var SMT = function () {
         var len = this.userData["edges"].length;
         var edges = this.userData["edges"];
 
-        //console.log("EdgeList ourfitnesscalc: ", edges);
         for (var i = 0; i < len; i++) {
             if (edges[i][2] > maxEdge)
                 maxEdge = edges[i][2];
@@ -286,17 +255,6 @@ var SMT = function () {
             graph.createEdge(GAEdges[i][1], GAEdges[i][0], GAEdges[i][2]);
         }
 
-        //console.log("Filtered edges: " + GAEdges);
-        //console.log("Connected edge length: " + GAEdges.length + " Sum: " + sumEdges + " Max: " + maxEdge);
-
-        //var RequiredNodeList = [
-        //    'transitional sensory area',
-        //    'supplementary sensory area',
-        //    'ventroposterior superior nucleus thalami',
-        //    'receptive field for the foot in area5',
-        //    'globus pallidus internal part'
-        //];
-
         var DisconnectedValuesList = new Array(this.RequiredNodeList.length + 1).join('0').split('').map(parseFloat);
 
         var SearchList = [];
@@ -313,7 +271,6 @@ var SMT = function () {
         for (var i = 0; i < this.RequiredNodeList.length; i++)
             SearchList[i] = this.RequiredNodeList[i];
 
-        //console.log("SearchList: " + SearchList);
         /*
          * For All SearchList[i] Not in Graph, assign MaxDisconnected
          * value to DisconnectedValueList and "true" to AttemptedList
@@ -326,7 +283,6 @@ var SMT = function () {
             }
         }
 
-        //console.log("DisconnectedValueList before: " + DisconnectedValuesList);
         /*
          * We should not have any nodes in required node list that do not
          * exist into the GA solution beyond this point.
@@ -339,7 +295,6 @@ var SMT = function () {
 
             AttemptedList[i] = true;
             FoundList = FoundList.concat(i);
-            //console.log(FoundList);
             for (var j = i + 1; j < SearchList.length; j++) {
                 /*
                  * If true Then this node is not in our Graph
@@ -353,7 +308,6 @@ var SMT = function () {
                 if (graph.hasPath(SearchList[i], SearchList[j])) {
                     FoundList = FoundList.concat(j);
                     AttemptedList[j] = true;
-                    //console.log(FoundList);
                 }
             }
 
@@ -366,10 +320,6 @@ var SMT = function () {
                 DisconnectedValuesList[FoundList[m]] = this.RequiredNodeList.length - FoundList.length;
             }
 
-            //console.log("DisconnectedValueList after: " + DisconnectedValuesList);
-            /*
-             * Empty FoundList
-             */
             FoundList = [];
         }
 
@@ -380,7 +330,6 @@ var SMT = function () {
         for (var i = 0; i < DisconnectedValuesList.length; i++)
             TotalDisconnected += DisconnectedValuesList[i];
 
-        //console.log("Chromosome: ", entity);
         console.log("sumEdges, TotalDisconnected, maxEdge: ", sumEdges, TotalDisconnected, maxEdge);
 
         fitness = sumEdges + ((maxEdge + 1) * TotalDisconnected * TotalDisconnected);
@@ -395,7 +344,7 @@ var SMT = function () {
     genetic.fitness = function (entity) {
         //importScripts("http://localhost:63342/SMT-Genetic/node_modules/graph.js/dist/graph.full.js");
 
-        return this.ourfitnesscalc(entity);
+        return this.OurFitnessFunction(entity);
     }
 
     /*
@@ -420,7 +369,7 @@ var SMT = function () {
 
         var result = [];
         console.log("GA RESULT: ", pop[0].entity);
-        console.log("GA FITNESS: ", this.ourfitnesscalc(pop[0].entity));
+        console.log("GA FITNESS: ", this.OurFitnessFunction(pop[0].entity));
         for (var i = 0; i < pop[0].entity.length; i++) {
             if (pop[0].entity[i] == 1) {
                 result.push([
@@ -432,28 +381,18 @@ var SMT = function () {
             }
         }
 
-        //console.log(result);
+        //added nodes from TempList as self edge
+        for (var i = 0; i < TempList.length; i++) {
+            result.push([TempList[i], TempList[i], 1, "macaque"])
+        }
+
         this.draw(result);
 
-        //console.log("MAX AND MIN: ", stats.maximum, stats.minimum);
-        //
-        //if (previousfitness != stats.maximum) {
-        //    previousfitness = stats.maximum;
-        //    fitnessunchanged = 0;
-        //}
-        //else {
-        //    fitnessunchanged++;
-        //    if(fitnessunchanged > 2){
-        //        isFinished = true;
-        //    }
-        //}
-        //
-        //console.log("MAX isFinished before", previousfitness, fitnessunchanged);
         if (isFinished) {
             console.log("isFinished ***************************");
 
             console.log("FINAL GA RESULT: ", pop[0].entity);
-            console.log("FINAL GA FITNESS: ", this.ourfitnesscalc(pop[0].entity));
+            console.log("FINAL GA FITNESS: ", this.OurFitnessFunction(pop[0].entity));
             for (var i = 0; i < pop[0].entity.length; i++) {
                 if (pop[0].entity[i] == 1) {
                     result.push([
@@ -463,6 +402,10 @@ var SMT = function () {
                         this.userData["edges"][i][3]
                     ])
                 }
+            }
+
+            for (var i = 0; i < TempList.length; i++) {
+                result.push([TempList[i], TempList[i], 1, "macaque"])
             }
 
             console.log("isFinished result: ", result);
@@ -620,15 +563,15 @@ var SMT = function () {
 
     //test-1 (Extension of GA sheet in google drive)
 
-    //var nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-    //    30, 31, 32, 33, 34, 35];
-    //var edges = [[1, 2, 1, "macaque"], [2, 29, 1, "Birds"], [2, 31, 1, "Rat"], [31, 32, 1, "Homo sapiens"], [1, 30, 1, "macaque"],
-    //    [1, 15, 1, "Birds"], [15, 16, 1, "Rat"], [15, 11, 1, "Homo sapiens"], [11, 12, 1, "macaque"], [12, 9, 1, "Birds"],
-    //    [12, 13, 1, "Rat"], [13, 7, 1, "Homo sapiens"], [13, 14, 1, "macaque"], [14, 8, 1, "Birds"], [5, 25, 1, "Homo sapiens"],
-    //    [25, 24, 1, "Homo sapiens"], [5, 26, 1, "macaque"], [6, 28, 1, "Birds"], [6, 23, 1, "Rat"], [23, 27, 1, "Homo sapiens"],
-    //    [3, 4, 1, "macaque"], [4, 35, 1, "Birds"], [4, 20, 1, "Homo sapiens"], [20, 22, 1, "Homo sapiens"], [4, 33, 1, "macaque"],
-    //    [33, 34, 1, "Birds"], [3, 10, 1, "Rat"], [10, 21, 1, "Homo sapiens"], [10, 17, 1, "macaque"], [17, 18, 1, "Birds"],
-    //    [18, 19, 1, "Rat"]];
+    var nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+        30, 31, 32, 33, 34, 35];
+    var edges = [[1, 2, 1, "macaque"], [2, 29, 1, "Birds"], [2, 31, 1, "Rat"], [31, 32, 1, "Homo sapiens"], [1, 30, 1, "macaque"],
+        [1, 15, 1, "Birds"], [15, 16, 1, "Rat"], [15, 11, 1, "Homo sapiens"], [11, 12, 1, "macaque"], [12, 9, 1, "Birds"],
+        [12, 13, 1, "Rat"], [13, 7, 1, "Homo sapiens"], [13, 14, 1, "macaque"], [14, 8, 1, "Birds"], [5, 25, 1, "Homo sapiens"],
+        [25, 24, 1, "Homo sapiens"], [5, 26, 1, "macaque"], [6, 28, 1, "Birds"], [6, 23, 1, "Rat"], [23, 27, 1, "Homo sapiens"],
+        [3, 4, 1, "macaque"], [4, 35, 1, "Birds"], [4, 20, 1, "Homo sapiens"], [20, 22, 1, "Homo sapiens"], [4, 33, 1, "macaque"],
+        [33, 34, 1, "Birds"], [3, 10, 1, "Rat"], [10, 21, 1, "Homo sapiens"], [10, 17, 1, "macaque"], [17, 18, 1, "Birds"],
+        [18, 19, 1, "Rat"]];
 
     //test-2 (Extension of GA sheet in google drive)
 
@@ -670,17 +613,17 @@ var SMT = function () {
 
     //Test-5 between SMT and GA-SMT
 
-    var nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
-        30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 111, 112];
-    var edges = [[1, 2, 21, "Birds"], [3, 10, 7, "Birds"], [10, 23, 2, "macaque"], [1, 14, 2, "macaque"], [32, 31, 5, "Rat"], [31, 6, 7, "Birds"],
-        [6, 7, 1, "Homo sapiens"], [14, 28, 5, "Rat"], [39, 15, 7, "Birds"], [1, 111, 7, "Birds"], [15, 14, 1, "Homo sapiens"],
-        [37, 41, 2, "macaque"], [41, 47, 5, "Rat"], [1, 13, 1, "Homo sapiens"], [13, 27, 5, "Rat"], [27, 26, 7, "Birds"],
-        [26, 12, 2, "macaque"], [112, 2, 7, "Birds"], [12, 2, 5, "Rat"], [8, 9, 5, "Rat"], [23, 22, 7, "Birds"], [22, 21, 2, "macaque"],
-        [12, 11, 7, "Birds"], [11, 25, 2, "macaque"], [11, 24, 5, "Rat"], [16, 4, 7, "Birds"], [4, 17, 2, "macaque"], [5, 17, 5, "Rat"],
-        [17, 18, 7, "Birds"], [17, 29, 2, "macaque"], [29, 38, 5, "Rat"], [29, 37, 7, "Birds"], [37, 40, 2, "macaque"], [40, 45, 5, "Rat"],
-        [40, 46, 7, "Birds"], [112, 111, 7, "Birds"], [41, 42, 7, "Birds"], [48, 49, 2, "macaque"], [49, 43, 5, "Rat"], [44, 50, 7, "Birds"],
-        [30, 34, 2, "macaque"], [34, 35, 5, "Rat"], [35, 36, 7, "Birds"], [33, 32, 2, "macaque"], [7, 3, 5, "Rat"], [19, 20, 5, "Rat"],
-        [20, 21, 7, "Birds"]];
+    //var nodes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29,
+    //    30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 111, 112];
+    //var edges = [[1, 2, 21, "Birds"], [3, 10, 7, "Birds"], [10, 23, 2, "macaque"], [1, 14, 2, "macaque"], [32, 31, 5, "Rat"], [31, 6, 7, "Birds"],
+    //    [6, 7, 1, "Homo sapiens"], [14, 28, 5, "Rat"], [39, 15, 7, "Birds"], [1, 111, 7, "Birds"], [15, 14, 1, "Homo sapiens"],
+    //    [37, 41, 2, "macaque"], [41, 47, 5, "Rat"], [1, 13, 1, "Homo sapiens"], [13, 27, 5, "Rat"], [27, 26, 7, "Birds"],
+    //    [26, 12, 2, "macaque"], [112, 2, 7, "Birds"], [12, 2, 5, "Rat"], [8, 9, 5, "Rat"], [23, 22, 7, "Birds"], [22, 21, 2, "macaque"],
+    //    [12, 11, 7, "Birds"], [11, 25, 2, "macaque"], [11, 24, 5, "Rat"], [16, 4, 7, "Birds"], [4, 17, 2, "macaque"], [5, 17, 5, "Rat"],
+    //    [17, 18, 7, "Birds"], [17, 29, 2, "macaque"], [29, 38, 5, "Rat"], [29, 37, 7, "Birds"], [37, 40, 2, "macaque"], [40, 45, 5, "Rat"],
+    //    [40, 46, 7, "Birds"], [112, 111, 7, "Birds"], [41, 42, 7, "Birds"], [48, 49, 2, "macaque"], [49, 43, 5, "Rat"], [44, 50, 7, "Birds"],
+    //    [30, 34, 2, "macaque"], [34, 35, 5, "Rat"], [35, 36, 7, "Birds"], [33, 32, 2, "macaque"], [7, 3, 5, "Rat"], [19, 20, 5, "Rat"],
+    //    [20, 21, 7, "Birds"]];
 
     //test-5 (Getting nodes and edges from data.json)
 
@@ -757,12 +700,6 @@ var SMT = function () {
     var ConnectedPaths = function () {
 
         var graph = new Graph();
-        //var edges = this.userData["edges"];
-        var chromosomes = [];
-
-        /*
-         * Populating edges including reverse edges
-         */
         for (var i = 0; i < edges.length; i++) {
             graph.createEdge(edges[i][0], edges[i][1], edges[i][2]);
             graph.createEdge(edges[i][1], edges[i][0], edges[i][2]);
@@ -775,15 +712,15 @@ var SMT = function () {
                 for (var it = graph.paths(RequiredNodeList[i], RequiredNodeList[j]), kv; !(kv = it.next()).done;) {
                     var path = kv.value;
                     paths.push([path]);
-                    chromosomes.push(chromos(paths.shift()));
+                    PathToEdges(paths.shift());
                 }
             }
         }
 
-        function chromos(path) {
+        function PathToEdges(path) {
             var myEdges = [];
             for (var i = 0; i < path.length; i++) {
-                if (path[i].length >= 2) {
+                if (path[i].length > 2) {
                     for (var j = 0; j < path[i].length - 1;) {
                         myEdges.push([path[i][j], path[i][++j]]);
                     }
@@ -796,25 +733,41 @@ var SMT = function () {
             while (myEdges.length != 0)
                 EdgeList.push(myEdges.shift());
 
-            console.log(myEdges);
         }
 
-        return EdgeList;
+        /*
+         * Utility function for removing non-unique edges from our solution
+         */
+        function uniqueify(es) {
+            var retval = [];
+            es.forEach(function (e) {
+                for (var j = 0; j < retval.length; j++) {
+                    if (retval[j][0] === e[0] && retval[j][1] === e[1])
+                        return;
+                }
+                retval.push(e);
+            });
+            return retval;
+        }
+
+        return uniqueify(EdgeList);
     }
 
     EdgeList = ConnectedPaths();
 
     /*
-     * adding weight of edges in EdgeList from the original dataset
+     * adding weight and species from the original dataset into our EdgeList
      */
     for (var i = 0; i < EdgeList.length; i++) {
         for (var j = 0; j < edges.length; j++) {
-            if ((EdgeList[i][0] == edges[j][0] && EdgeList[i][1] == edges[j][1]) || (EdgeList[i][1] == edges[j][0] && EdgeList[i][0] == edges[j][1]))
+            if ((EdgeList[i][0] == edges[j][0] && EdgeList[i][1] == edges[j][1]) || (EdgeList[i][1] == edges[j][0] && EdgeList[i][0] == edges[j][1])) {
                 EdgeList[i][2] = edges[j][2];
+                EdgeList[i][3] = edges[j][3];
+            }
         }
     }
 
-    console.log("Only edges: ", EdgeList);
+    console.log("EdgeList: ", EdgeList);
     /*
      * Initial GA input
      */
@@ -834,7 +787,7 @@ var SMT = function () {
     var end = new Date().getTime();
 
     /*
-     * Measure only execuation time of GA. Skip execution
+     * Only measure execution time of GA. Skip execution
      * of Draw function from genetic.notification. Fix this!!
      */
     console.log("Execution Time: ", end - start);
